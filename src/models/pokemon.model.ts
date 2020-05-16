e4342e10b02283000fbc5ea54fc3f3cb97f6cd90
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-05-16 16:14:04 +0700

    init: pokemon model

---
+++ src/models/pokemon.model.ts
@@ -0,0 +1,11 @@
+import mongoose, { Mongoose } from 'mongoose';
+const PokemonSchema = new mongoose.Schema({
+    name: String,
+    gender: String,
+    type: String,
+    height: Number,
+    weight: Number,
+    photo: String
+});
+
+export const Pokemon = mongoose.model("Pokemon", PokemonSchema);
