dbef2a3f478c1b1e2c92271decd30b996a9891be
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-05-16 16:14:14 +0700

    init: mongodb conenction

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import { Application } from 'express';
 import bodyParser from "body-parser";
 import cors from "cors";
 
+import { MONGO_URL } from './constants/pokeApi.constants';
+import mongoose from "mongoose";
+
 import { Controller } from './controllers/main.controller';
 
 class App {
@@ -12,19 +15,20 @@ class App {
     constructor() {
         this.app = express();                        
         this.setConfig();
+        this.setMongoConfig();
 
         this.pokeController = new Controller(this.app);
     }
     
     private setConfig() {
-        //Allows us to receive requests with data in json format
-        this.app.use(bodyParser.json({ limit: "50mb" }));
-                        
-        //Allows us to receive requests with data in x-www-form-urlencoded format
-        this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+        this.app.use(bodyParser.json({ limit: "50mb" })); // Allows us to receive requests with data in json format
+        this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); // Allows us to receive requests with data in x-www-form-urlencoded format
+        this.app.use(cors()); // Enables cors
+    }
 
-        //Enables cors
-        this.app.use(cors());
+    private setMongoConfig() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     }
 }
 
